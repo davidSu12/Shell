e94183fca42740760c597e657390b6de14b2585a
davidSu12 <david.suarez0501@gmail.com>
2025-08-20 20:23:07 +0200

    [ENTRY] Added a simple parser for the entry

--- main.asm
+++ main.asm
@@ -1,4 +1,5 @@
 %define LEN_BUFFER 512
+%define MAX_TROZOS 64 
 
 %macro endProgram 0
     mov rax, 60
@@ -7,7 +8,6 @@
 %endmacro
 
 %macro printNewLine 0
-    
     mov rax, 1
     mov rdi, 1
     mov rsi, new_line
@@ -15,14 +15,27 @@
     syscall
 %endmacro
 
+
+%macro printPrompt 0
+
+    push prompt_length
+    push prompt
+    call printString
+
+%endmacro
+
 section .data
 
 
-prompt db  "$>", 0
-prompt_length equ $ - prompt
-terminado db 0
-new_line db 0Ah
-buffer db  LEN_BUFFER dup(0)
+prompt db  "$>"                               ;prompt
+prompt_length equ $ - prompt                       ;len prompt
+
+new_line db 0Ah                                         ;caracter new line
+terminado db 0                                          ;variable terminado, termina el bucle del buffer
+
+comando db  LEN_BUFFER dup(0)                           ;buffer para guardar la entrada del comando
+comando_trozos dq MAX_TROZOS dup(0)                     ;buffer para guardar los trozos del comando
+comando_copy db LEN_BUFFER dup(0)
 
 section .bss
 
@@ -30,7 +43,7 @@ section .bss
 section .text
 global _start
 
-
+; funcion que sirve para imprimir un string
 printString:
     push rbp
     mov rbp, rsp
@@ -51,18 +64,75 @@ _start:
     cmp byte [terminado],  0
     jne _endwhile
 
-    push prompt_length
-    push prompt
-    call printString
+    printPrompt
 
+    ;leemos entrada
     mov rax, 0
     mov rdi, 0
-    mov rsi, buffer
+    mov rsi, comando
     mov rdx, LEN_BUFFER
     syscall
 
+
+    ;copiamos de un buffer a otro
+    mov esi, comando
+    mov edi, comando_copy
+    mov rcx, rax
+    rep movsb
+
+    ;acto seguido hacemos el parsing
+    mov rbx, comando_copy                           ;init lexeme
+    mov rcx, comando_copy                           ;current lexeme
+    mov rsi, comando_trozos                         ;aqui vamos a ir guardando los trozos
+    
+
+    _while1:
+    cmp byte [rbx], 0
+    je _endwhile
+
+
+    ;lo expresamos como dos ifs
+    
+    cmp byte [rcx], ' '
+    jne _false
+    
+    _true:    
+    mov byte [rcx], 0
+    mov qword[rsi], rbx
+    add rsi, 8
+    inc rcx
+    
+    _while2:
+    cmp byte[rcx], ' '
+    jne _endwhile2
+    inc rcx
+    jmp _while2
+    
+    _endwhile2:
+    mov rbx, rcx
+    jmp _next
+    _false:
+    _next:
+
+
+    cmp byte[rcx], 0
+    jne _false1
+
+    _true1:
+    mov qword[rsi], rbx
+    jmp _next1
+    
+    _false1:
+    _next1:
+    jmp _while1
+    _endwhile1:
+
+
+
+
+
     push LEN_BUFFER
-    push buffer
+    push comando_copy
     call printString
 
     printNewLine
